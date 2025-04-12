8106616e2999a6ac75eb900d23c9048e78d51e25
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2025-04-12 20:11:46 +0530

    Initial Commit

---
+++ CircularCountdown/CircularCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularCountdownApp.swift
+//  CircularCountdown
+//
+//  Created by Rahul Ravi Prakash on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CircularCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
